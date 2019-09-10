9f034f7757d85d8166b5922a12a91d81d6b037ab
Siavoosh Ahmadpoorkhani <siavash.ahmadpoor@gmail.com>
2019-09-10 22:19:55 +0430

    Update search.php
    added c parameter for the page counter.
    for example : 
    search.php?q=spam&p=1&c=4
    searches for "spam" from page one to 4 pages later.
    note: only q parameter is required, p and c have a default value and you don't have to use them both

--- search.php
+++ search.php
@@ -1,23 +1,30 @@
 <?php
-if(isset($_GET['q']) && !empty($_GET['q'])){
-	
-	$page = isset($_GET['p']) && !empty($_GET['p']) && is_int((int)$_GET['p']) && (int)$_GET['p'] > 0 ? ((int)$_GET['p'] - 1) * 10 : 0; 
-	$url = "https://google.com/search?q=" . urlencode($_GET['q']) . "&start=" . $page;
-	$html = utf8_encode(file_get_contents($url));
-	$pattern = '~[a-z]+://\S+~';
-	$links = [];
-	if($num_found = preg_match_all($pattern, $html, $out))
-	{
-	  foreach($out[0] as $l){
-		  $l = str_replace("<","",$l);
-		  $url_info = parse_url($l);
-		  if(!strpos($l,"google") && !in_array($url_info['scheme'] . '://' . $url_info['host'],$links))
-			  $links[] = $url_info['scheme'] . '://' . $url_info['host'];
-	  }
-	}
-	echo "<pre>";
-	print_r($links);
-	echo "</pre>";
-}
-else
-	echo 'missed q parameter';
+if (isset($_GET['q']) && !empty($_GET['q'])) {
+
+    define("PER_PAGE", 10);
+    $page = isset($_GET['p']) && !empty($_GET['p']) && is_int((int)$_GET['p']) && (int)$_GET['p'] > 0 ? (int)$_GET['p'] - 1 : 0;
+    $count = isset($_GET['c']) && !empty($_GET['c']) && is_int((int)$_GET['c']) && (int)$_GET['c'] > 0 ? ((int)$_GET['c']) : 0;
+
+    $html = "";
+    
+    do {
+        $url = "https://google.com/search?q=" . urlencode($_GET['q']) . "&start=" . ($page++) * PER_PAGE;
+        $html .= utf8_encode(file_get_contents($url));
+    } while ($count--);
+
+    $pattern = '~[a-z]+://\S+~';
+    $links = [];
+    if ($num_found = preg_match_all($pattern, $html, $out)) {
+        foreach ($out[0] as $l) {
+            $l = str_replace("<", "", $l);
+            $url_info = parse_url($l);
+            if (!strpos($l, "google") && !in_array($url_info['scheme'] . '://' . $url_info['host'], $links))
+                $links[] = $url_info['scheme'] . '://' . $url_info['host'];
+        }
+    }
+
+    echo "<pre>";
+    print_r($links);
+    echo "</pre>";
+} else
+    echo 'missed q parameter';
